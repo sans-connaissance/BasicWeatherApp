21020336712cf442e36b34569efa53f8d36d8fa3
sans-connaissance <davemalicke@gmail.com>
2021-10-19 12:29:01 -0400

    Initial Commit

---
+++ BasicWeatherApp/BasicWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicWeatherAppApp.swift
+//  BasicWeatherApp
+//
+//  Created by David Malicke on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BasicWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
